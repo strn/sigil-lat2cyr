504f28504383c242865764c6be9ebf32a19046f3
Strn <strn@github.com>
2022-07-16 00:24:07 +0200

    Initial commit

---
+++ lib/py2srbcyr.py
@@ -0,0 +1,811 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__doc__ = """
+Program for transliteration of texts written in Latin alphabet into Serbian Cyrillic.
+
+Python implementation of great Javascript program: https://github.com/turanjanin/cirilizator .
+"""
+
+import re
+
+
+class SerbCyr:
+
+    # Digraphs must be placed first
+    _initial_map = {
+        "DJ": "Ђ",
+        "DЈ": "Ђ", # D + cyrillic J
+        "Dj": "Ђ",
+        "Dј": "Ђ", # D + cyrillic j
+        "LJ": "Љ",
+        "LЈ": "Љ", # L + cyrillic J
+        "Ǉ":  "Љ",
+        "Lj": "Љ",
+        "Lј": "Љ", # L + cyrillic j
+        "ǈ":  "Љ",
+        "NJ": "Њ",
+        "NЈ": "Њ", # N + cyrillic J
+        "Ǌ":  "Њ",
+        "Nj": "Њ",
+        "Nј": "Њ", # N + cyrillic j
+        "ǋ":  "Њ",
+        "DŽ": "Џ",
+        "Ǆ":  "Џ",
+        "DŽ": "Џ", # D + Z with caron
+        "Dž": "Џ",
+        "ǅ":  "Џ",
+        "Dž": "Џ", # D + z with caron
+        "dj": "ђ",
+        "dј": "ђ", # d + cyrillic j
+        "lj": "љ",
+        "lј": "љ", # l + cyrillic j
+        "ǉ":  "љ",
+        "nj": "њ",
+        "nј": "њ", # n + cyrillic j
+        "ǌ":  "њ",
+        "dž": "џ",
+        "ǆ":  "џ",
+        "dž": "џ", # d + z with caron
+        "A":  "А",
+        "B":  "Б",
+        "V":  "В",
+        "G":  "Г",
+        "D":  "Д",
+        "Đ":  "Ђ",
+        "Ð":  "Ђ",
+        "ᴆ":  "Ђ",
+        "E":  "Е",
+        "Ž":  "Ж",
+        "Ž":  "Ж", # Z with caron
+        "Z":  "З",
+        "I":  "И",
+        "J":  "Ј",
+        "K":  "К",
+        "L":  "Л",
+        "M":  "М",
+        "N":  "Н",
+        "O":  "О",
+        "P":  "П",
+        "R":  "Р",
+        "S":  "С",
+        "T":  "Т",
+        "Ć":  "Ћ",
+        "Ć":  "Ћ", # C with acute accent
+        "U":  "У",
+        "F":  "Ф",
+        "H":  "Х",
+        "C":  "Ц",
+        "Č":  "Ч",
+        "Č":  "Ч", # C with caron
+        "Š":  "Ш",
+        "Š":  "Ш", # S with caron
+        "a":  "а",
+        "æ":  "ае",
+        "b":  "б",
+        "v":  "в",
+        "g":  "г",
+        "d":  "д",
+        "đ":  "ђ",
+        "e":  "е",
+        "ž":  "ж",
+        "ž":  "ж", # z with caron
+        "z":  "з",
+        "i":  "и",
+        "ĳ":  "иј",
+        "j":  "ј",
+        "k":  "к",
+        "l":  "л",
+        "m":  "м",
+        "n":  "н",
+        "o":  "о",
+        "œ":  "ое",
+        "p":  "п",
+        "r":  "р",
+        "s":  "с",
+        "ﬆ":  "ст",
+        "t":  "т",
+        "ć":  "ћ",
+        "ć":  "ћ", # c with acute accent
+        "u":  "у",
+        "f":  "ф",
+        "ﬁ":  "фи",
+        "ﬂ":  "фл",
+        "h":  "х",
+        "c":  "ц",
+        "č":  "ч",
+        "č":  "ч", # c with caron
+        "š":  "ш",
+        "š":  "ш", # s with caron
+    }
+
+    _serbian_words_with_foreign_character_combinations = [
+        "ammar",
+        "amss",
+        "aparthejd",
+        "ddor",
+        "dss",
+        "dvadesettrog",
+        "epp",
+        "fss",
+        "gricka",
+        "gss",
+        "interreakc",
+        "interresor",
+        "ishran",
+        "izzzdiovns",
+        "kock",
+        "kss",
+        "llsls",
+        "mmf",
+        "naddu",
+        "natha",
+        "natho",
+        "neshvać",
+        "obeshrabr",
+        "ommetar",
+        "ophod",
+        "penthaus",
+        "palack",
+        "pecka",
+        "poddirektor",
+        "poddisciplin",
+        "poddomen",
+        "poddres",
+        "poodmak",
+        "posthumn",
+        "posttrans",
+        "posttraum",
+        "pothodni",
+        "pothranj",
+        "pothva",
+        "preddijabetes",
+        "prekookean",
+        "prethod",
+        "ptt",
+        "reck", 
+        "rezucka",
+        "sbb",
+        "sdss",
+        "shvać",
+        "shvat",
+        "skocka",
+        "ssp",
+        "ssrnj",
+        "sssr",
+        "superračun",
+        "strugucka",
+        "šopingholi",
+        "tass",
+        "trocki",
+        "transseks",
+        "transsibir",
+        "tridesettrog",
+        "uppr",
+        "ushiće",
+        "vannastav",
+        "zamuck",
+        "zoo"
+    ]
+
+    _common_foreign_words = [
+        "administration",
+        "adobe",
+        "advanced",
+        "advertising",
+        "autocad",
+        "bitcoin",
+        "book",
+        "boot",
+        "cancel",
+        "canon",
+        "carlsberg",
+        "cisco",
+        "clio",
+        "cloud",
+        "coca-col",
+        "cookie",
+        "cooking",
+        "cool",
+        "covid",
+        "dacia",
+        "default",
+        "develop",
+        "e-mail",
+        "edge",
+        "email",
+        "emoji",
+        "english",
+        "facebook",
+        "fashion",
+        "food",
+        "foundation",
+        "gaming",
+        "gmail",
+        "gmt",
+        "good",
+        "google",
+        "hdmi",
+        "image",
+        "iphon",
+        "ipod",
+        "javascript",
+        "jazeera",
+        "joomla",
+        "league",
+        "like",
+        "linkedin",
+        "look",
+        "macbook",
+        "mail",
+        "manager",
+        "maps",
+        "mastercard",
+        "mercator",
+        "microsoft",
+        "mitsubishi",
+        "notebook",
+        "nvidia",
+        "online",
+        "outlook",
+        "panasonic",
+        "pdf",
+        "peugeot",
+        "podcast",
+        "postpaid",
+        "printscreen",
+        "procredit",
+        "project",
+        "punk",
+        "renault",
+        "rock",
+        "screenshot",
+        "seen",
+        "selfie",
+        "share",
+        "shift",
+        "shop",
+        "smartphone",
+        "space",
+        "steam",
+        "stream",
+        "subscrib",
+        "tool",
+        "topic",
+        "trailer",
+        "ufc",
+        "unicredit",
+        "username",
+        "viber"
+    ]
+
+    _whole_foreign_words = [
+        "again",
+        "air",
+        "alpha",
+        "and",
+        "back",
+        "bitcoin",
+        "blue",
+        "celebrities",
+        "conditions",
+        "co2",
+        "cpu",
+        "creative",
+        "disclaimer",
+        "dj",
+        "electronics",
+        "fresh",
+        "fun",
+        "geographic",
+        "gmbh",
+        "h2o",
+        "hair",
+        "have",
+        "home",
+        "ii",
+        "iii",
+        "idj",
+        "idjtv",
+        "life",
+        "live",
+        "login",
+        "national",
+        "made",
+        "makeup",
+        "must",
+        "previous",
+        "public",
+        "reserved",
+        "terms",
+        "url",
+        "vii",
+        "viii",
+        "visa"
+    ]
+
+    _foreign_character_combinations = [
+        'q',
+        'w',
+        'x',
+        'y',
+        'é',
+        'á',
+        'ó',
+        'ü',
+        'ö',
+        'ä',
+        'ê',
+        'è',
+        'ú',
+        'í',
+        'ő',
+        'ű',
+        'ñ',
+        'ş',
+        'ç',
+        'ğ',
+        'ı',
+        'ł',
+        'ý',
+        'ø',
+        'ß',
+        '&',
+        '@',
+        '#',
+        'bb',
+        'cc',
+        'ck',
+        'cs',
+        'dd',
+        'ee',
+        'ff',
+        'gg',
+        'gy',
+        'hh',
+        'ie',
+        'kk',
+        'll',
+        'ly',
+        'mm',
+        'nn',
+        'ny',
+        'oo',
+        'ph',
+        'pp',
+        'rr',
+        'sh',
+        'ss',
+        'sz',
+        'tt',
+        'uu',
+        'zh',
+        'zs',
+        'zz',
+        'ch',
+        'gh',
+        'th',
+        '\'s',
+        '\'t',
+        '.com',
+        '.edu',
+        '.net',
+        '.info',
+        '.rs',
+        '.org',
+        '©',
+        '®',
+        '™'
+    ]
+
+    _digraph_exceptions = {
+        "dj": [
+            "adjektiv",
+            "adjunkt",
+            "bazdje",
+            "bdje",
+            "bezdje",
+            "blijedje",
+            "bludje",
+            "bridjе",
+            "vidjel",
+            "vidjet",
+            "vindjakn",
+            "višenedje",
+            "vrijedje",
+            "gdje",
+            "gudje",
+            "gdjir",
+            "daždje",
+            "dvonedje",
+            "devetonedje",
+            "desetonedje",
+            "djb",
+            "djeva",
+            "djevi",
+            "djevo",
+            "djed",
+            "djejstv",
+            "djel",
+            "djenem",
+            "djeneš",
+            # "djene" rare (+ Дјене (town)), but it would colide with ђене-ђене, ђеневљанка, ђенерал итд.
+            "djenu",
+            "djet",
+            "djec",
+            "dječ",
+            "djuar",
+            "djubison",
+            "djubouz",
+            "djuer",
+            "djui",
+            # "djuk", djuk (engl. Duke) косило би се нпр. са Djukanović
+            "djuks",
+            "djulej",
+            "djumars",
+            "djupont",
+            "djurant",
+            "djusenberi",
+            "djuharst",
+            "djuherst",
+            "dovdje",
+            "dogrdje",
+            "dodjel",
+            "drvodje",
+            "drugdje",
+            "elektrosnabdje",
+            "žudje",
+            "zabludje",
+            "zavidje",
+            "zavrijedje",
+            "zagudje",
+            "zadjev",
+            "zadjen",
+            "zalebdje",
+            "zaludje",
+            "zaodje",
+            "zapodje",
+            "zarudje",
+            "zasjedje",
+            "zasmrdje",
+            "zastidje",
+            "zaštedje",
+            "zdje",
+            "zlodje",
+            "igdje",
+            "izbledje",
+            "izblijedje",
+            "izvidje",
+            "izdjejst",
+            "izdjelj",
+            "izludje",
+            "isprdje",
+            "jednonedje",
+            "kojegdje",
+            "kudjelj",
+            "lebdje",
+            "ludjel",
+            "ludjet",
+            "makfadjen",
+            "marmadjuk",
+            "međudjel",
+            "nadjaha",
+            "nadjača",
+            "nadjeb",
+            "nadjev",
+            "nadjenul",
+            "nadjenuo",
+            "nadjenut",
+            "negdje",
+            "nedjel",
+            "nadjunač",
+            "nenadjača",
+            "nenadjebi",
+            "nenavidje",
+            "neodje",
+            "nepodjarm",
+            "nerazdje",
+            "nigdje",
+            "obdjel",
+            "obnevidje",
+            "ovdje",
+            "odjav",
+            "odjah",
+            "odjaš",
+            "odjeb",
+            "odjev",
+            "odjed",
+            "odjezd",
+            "odjek",
+            "odjel",
+            "odjen",
+            "odjeć",
+            "odjec",
+            "odjur",
+            "odsjedje",
+            "ondje",
+            "opredje",
+            "osijedje",
+            "osmonedje",
+            "pardju",
+            "perdju",
+            "petonedje",
+            "poblijedje",
+            "povidje",
+            "pogdjegdje",
+            "pogdje",
+            "podjakn",
+            "podjamč",
+            "podjastu",
+            "podjemč",
+            "podjar",
+            "podjeb",
+            "podjed",
+            "podjezič",
+            "podjel",
+            "podjen",
+            "podjet",
+            "pododjel",
+            "pozavidje",
+            "poludje",
+            "poljodjel",
+            "ponegdje",
+            "ponedjelj",
+            "porazdje",
+            "posijedje",
+            "posjedje",
+            "postidje",
+            "potpodjel",
+            "poštedje",
+            "pradjed",
+            "prdje",
+            "preblijedje",
+            "previdje",
+            "predvidje",
+            "predjel",
+            "preodjen",
+            "preraspodje",
+            "presjedje",
+            "pridjev",
+            "pridjen",
+            "prismrdje",
+            "prištedje",
+            "probdje",
+            "problijedje",
+            "prodjen",
+            "prolebdje",
+            "prosijedje",
+            "prosjedje",
+            "protivdjel",
+            "prošlonedje",
+            "radjard",
+            "razvidje",
+            "razdjev",
+            "razdjel",
+            "razodje",
+            "raspodje",
+            "rasprdje",
+            "remekdjel",
+            "rudjen",
+            "rudjet",
+            "sadje",
+            "svagdje",
+            "svidje",
+            "svugdje",
+            "sedmonedjelj",
+            "sijedje",
+            "sjedje",
+            "smrdje",
+            "snabdje",
+            "snovidje",
+            "starosjedje",
+            "stidje",
+            "studje",
+            "sudjel",
+            "tronedje",
+            "ublijedje",
+            "uvidje",
+            "udjel",
+            "udjen",
+            "uprdje",
+            "usidjel",
+            "usjedje",
+            "usmrdje",
+            "uštedje",
+            "cjelonedje",
+            "četvoronedje",
+            "čukundjed",
+            "šestonedjelj",
+            "štedje",
+            "štogdje",
+            "šukundjed",
+        ],
+        "dž": [
+            "feldžandarm",
+            "nadžanj",
+            "nadždrel",
+            "nadžel",
+            "nadžeo",
+            "nadžet",
+            "nadživ",
+            "nadžinj",
+            "nadžnj",
+            "nadžrec",
+            "nadžup",
+            "odžali",
+            "odžari",
+            "odžel",
+            "odžive",
+            "odživljava",
+            "odžubor",
+            "odžvaka",
+            "odžval",
+            "odžvać",
+            "podžanr",
+            "podžel",
+            "podže",
+            "podžig",
+            "podžiz",
+            "podžil",
+            "podžnje",
+            "podžupan",
+            "predželu",
+            "predživot",
+        ],
+        "nj": [
+            "anjon",
+            "injaric",
+            "injekc",
+            "injekt",
+            "injicira",
+            "injurij",
+            "kenjon",
+            "konjug",
+            "konjunk",
+            "nekonjug",
+            "nekonjunk",
+            "ssrnj",
+            "tanjug",
+            "vanjezičk",
+        ],
+    }
+
+    # See: https://en.wikipedia.org/wiki/Zero-width_non-joiner
+    _digraph_replacements = {
+        "dj": {
+            "dj": "d\u200Cj",
+            "Dj": "D\u200Cj",
+            "DJ": "D\u200CJ",
+        },
+        "dž": {
+            "dž": "d\u200Cž",
+            "Dž": "D\u200Cž",
+            "DŽ": "D\u200CŽ",
+        },
+        "nj": {
+            "nj": "n\u200Cj",
+            "Nj": "N\u200Cj",
+            "NJ": "N\u200CJ",
+        }
+    }
+
+    def text_to_cyrillic(self, text):
+        if len(text.strip()) == 0:
+            return text
+        words = re.split('\s+', text)
+        for i in range(len(words)):
+            index = self._transliteration_index_of_word_starts_with(words[i], self._whole_foreign_words, "-")
+            if index >= 0:
+                words[i] = words[i][:index] + self._word_to_cyrillic(words[i][index:])
+            else:
+                if not self._looks_like_foreign_word(words[i]):
+                    words[i] = self._word_to_cyrillic(words[i])
+        return ' '.join(words)
+
+
+    def _looks_like_foreign_word(self, word):
+        trimmed_word = self._trim_excessive_characters(word)
+        word = trimmed_word.lower()
+
+        if word == "":
+            return False
+
+        if self._word_starts_with(word, self._serbian_words_with_foreign_character_combinations):
+            return False
+
+        if self._word_contains_string(word, self._foreign_character_combinations):
+            return True
+
+        if self._word_starts_with(word, self._common_foreign_words):
+            return True
+
+        if self._word_is_equal_to(word, self._whole_foreign_words):
+            return True
+
+        if self._word_contains_measurement_unit(trimmed_word):
+            return True
+
+        return False
+
+
+    def _word_to_cyrillic(self, word):
+        word = self._split_latin_digraphs(word)
+        for key, value in self._initial_map.items():
+            word = word.replace(key, value)
+        return word
+
+
+    def _split_latin_digraphs(self, str1):
+        lowercaseStr = str1.strip().lower()
+
+        for digraph in self._digraph_exceptions:
+            if not digraph in lowercaseStr:
+                continue
+
+            for word in self._digraph_exceptions[digraph]:
+                if not lowercaseStr.startswith(word):
+                    continue
+
+                # Split all possible occurrences, regardless of case
+                for key in self._digraph_replacements[digraph]:
+                    str1 = str1.replace(key, self._digraph_replacements[digraph][key])
+
+                break
+        return str1
+
+
+    def _word_contains_string(self, word, array):
+        for array_word in array:
+            if array_word in word:
+                return True
+        return False
+
+    def _word_is_equal_to(self, word, array):
+        for array_word in array:
+            if word == array_word:
+                return True
+        return False
+
+    def _word_starts_with(self, word, array):
+        for array_word in array:
+            if word.startswith(array_word):
+                return True
+        return False
+
+    def _word_contains_measurement_unit(self, word):
+        unit_adjacent_to_sth = "([zafpnμmcdhKMGTPEY]?([BVWJFSHCΩATNhlmg]|m[²³]?|s[²]?|cd|Pa|Wb|Hz))"
+        unit_optionaly_adjacent_to_sth = "(°[FC]|[kMGTPZY](B|Hz)|[pnμmcdhk]m[²³]?|m[²³]|[mcdh][lg]|kg|km)"
+        number = "(\d+([\.,]\d)*)"
+        regExp = "^(" + number + unit_adjacent_to_sth + ")|(" \
+            + number + "?(" + unit_optionaly_adjacent_to_sth + "|" \
+            + unit_adjacent_to_sth + "/" + unit_adjacent_to_sth + "))$"
+
+        return re.match(regExp, word) is not None
+
+
+    """
+    Retrieves index of the first character of the word that should be transliterated.
+    Function examines only words that have a root that is a foreign word, followed by
+    some separator character and remainder of the word which is in Serbian.
+    Example: dj-evi should be transliterated as dj-еви so the function retrieves 3.
+    """
+    def _transliteration_index_of_word_starts_with(self, word, array, char_separator):
+        word = self._trim_excessive_characters(word).lower()
+        if word == "":
+            return -1
+
+        appended_foreign_words = list(map(lambda el: el + char_separator, array))
+
+        for array_word in appended_foreign_words:
+            if word.startswith(array_word):
+                return len(array_word)
+
+        return -1
+
+
+    # Trims white spaces and punctuation marks from the start and the end of the word.
+    def _trim_excessive_characters(self, word):
+        excessive_chars = "[\\s!?,:;\.\*\\-—~`'\"„”“”‘’(){}\\[\\]<>«»\\/\\\\]"
+        regexp = "^(" + excessive_chars + ")+|(" + excessive_chars + ")+$"
+
+        return re.sub(regexp, '', word)
---
+++ plugin.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
+
+from   datetime  import datetime
+from   lxml      import etree
+import lib.py2srbcyr as pycir
+
+# Global variables
+MODNAME = 'Lat2Cyr'
+# HTML tags that can contain text requireing transliteration
+HTML_TAGS = ('a', 'div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'img', 'p', 'br', 'b', 'i', 'em', 'span', 'sub', 'sup', 'title', 'th', 'td', 'li', 'strong', 'u')
+GLOBAL_HTML4_ATTRS = ('accesskey', 'class', 'dir', 'id', 'lang', 'style', 'tabindex', 'title',)
+# Dictionary that specifies which attributes are supported by which HTML tags
+ALLOWED_ATTRIBS = {
+	'br' : GLOBAL_HTML4_ATTRS,
+	'body' : GLOBAL_HTML4_ATTRS,
+	'table' : ('align', 'bgcolor', 'border', 'cellpadding', 'cellspacing', 'frame', 'rules', 'sortable', 'summary', 'width',),
+	'td' : ('abbr', 'align', 'axis', 'bgcolor', 'char', 'charoff', 'colspan', 'headers', 'height', 'nowrap', 'rowspan', 'scope', 'valign', 'width',) + GLOBAL_HTML4_ATTRS,
+	'tr' : ('align', 'bgcolor', 'char', 'charoff', 'valign',) + GLOBAL_HTML4_ATTRS,
+	'img' : ( 'alt', 'class', 'ismap', 'src', 'style', 'width',) + GLOBAL_HTML4_ATTRS,
+	'svg' : ( 'xmlns', 'xmlns:link', 'height', 'version', 'width',)
+}
+# Dictionary that says what attribute can be added to what tag, if missing
+ADD_IF_MISSING_ATTRS = {
+	'img' : ( ('alt', 'img'), )
+}
+# Special tags for eBook files
+EBOOK_TAGS = ( 'text', 'creator', 'contributor', 'description', 'meta', 'publisher', 'subject', 'title' )
+EBOOK_TAGS_ATTRIBUTES = { 'meta' : { 'name' : ('calibre:series', 'calibre:title_sort') } }
+HTML_DOCTYPE = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+"""
+NCX_DOCTYPE = """<!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN"
+   "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
+"""
+
+
+# Core function that converts text in HTML elements
+# from Croatian Latin into Serbian Cyrillic script
+def html_lat2cyr(tree, cyr, doctype):
+    ts = datetime.now().strftime("%Y-%m-%d %H:%M")
+    # Add META tag with correct encoding
+    meta_el = tree.xpath("//meta[@charset or @content]")
+    if not meta_el:
+        # Add META tags that define content
+        head_elem = tree.find('head')
+        if head_elem is not None:
+            metachr = etree.SubElement(head_elem, 'meta')
+            metachr.set(u'http-equiv', u"content-type")
+            metachr.set(u'content', u"text/html; charset=utf-8")
+            metachr.text = ''
+
+    # Walk over tree, changing text nodes
+    for elem in tree.getiterator():
+        if elem.tag in HTML_TAGS:
+            if elem.text is not None:
+                elem.text = cyr.text_to_cyrillic(elem.text)
+            if elem.tail is not None:
+                elem.tail = cyr.text_to_cyrillic(elem.tail)
+        if elem.tag in ADD_IF_MISSING_ATTRS:
+            for (attr, values) in ADD_IF_MISSING_ATTRS.items():
+                for val in values:
+                    if val[0] not in elem.attrib.keys():
+                        # print(f"Element '{elem.tag}' is missing attribute {attr}")
+                        elem.attrib[val[0]] = val[1]
+        # Check if all attributes are in list of allowed/supported
+        if elem.tag in ALLOWED_ATTRIBS.keys():
+            for attr in elem.attrib.keys():
+                if attr not in ALLOWED_ATTRIBS[elem.tag]:
+                    del elem.attrib[ attr ]
+        if elem.tag == 'html':
+            # Replace existing 'lang' and 'xml:lang' attributes
+            elem.attrib['lang'] = 'sr'
+            elem.attrib['xml:lang'] = 'sr'
+
+    if not has_translit_comment(tree):
+        tree.append(etree.Comment(f"Пресловљено програмом-додатком '{MODNAME}'; време: {ts}"))
+    return etree.tostring(tree, pretty_print=True, xml_declaration=True,
+        doctype=doctype, encoding='utf-8')
+
+
+def has_translit_comment(tree):
+    comments = tree.xpath('//comment()')
+    for c in comments:
+        if c.text.find(MODNAME) > -1:
+            return True
+    return False
+
+# Core function that converts text in XML elements
+# from Croatian Latin into Serbian Cyrillic script
+def xml_lat2cyr(tree, cyr, doctype=None):
+    ts = datetime.now().strftime("%Y-%m-%d %H:%M")
+    # Walk over tree, changing text nodes
+    for elem in tree.getiterator():
+        # Remove namespace
+        if not isinstance(elem.tag, str):
+            continue
+        leftbr = elem.tag.find('{')
+        rightbr = elem.tag.find('}')
+        if leftbr > -1 and rightbr > -1 and rightbr > leftbr:
+            tag = elem.tag[rightbr+1:]
+        else:
+            tag = elem.tag
+
+        if tag in EBOOK_TAGS:
+            if elem.text is not None:
+                elem.text = cyr.text_to_cyrillic(elem.text)
+            # Convert some attributes
+            if tag == 'meta' and 'name' in elem.attrib.keys():
+                condList = EBOOK_TAGS_ATTRIBUTES['meta']['name']
+                if elem.attrib['name'] in condList:
+                    elem.attrib['content'] = cyr.text_to_cyrillic(elem.attrib['content'])
+        elif tag == 'language':
+            elem.text = 'sr'
+    if not has_translit_comment(tree):
+        tree.append(etree.Comment(f"Пресловљено програмом-додатком '{MODNAME}'; време {ts}"))
+    return etree.tostring(tree, pretty_print=True, xml_declaration=True, encoding='utf-8', doctype=doctype)
+
+
+def translit_toc(bk, xml_parser, cyr):
+    # Transliterate ToC
+    ncx_id = bk.gettocid()
+    source = bk.readfile(ncx_id)
+    tree = etree.XML(source.encode('utf-8'), xml_parser)
+    transliterated = xml_lat2cyr(tree, cyr, NCX_DOCTYPE)
+    bk.writefile(ncx_id, transliterated)
+    print(f"Пресловљен садржај књиге")
+
+
+def translit_metadata(bk, xml_parser, cyr):
+    source = bk.getmetadataxml()
+    tree = etree.XML(source.encode('utf-8'), xml_parser)
+    transliterated = xml_lat2cyr(tree, cyr)
+    bk.setmetadataxml(transliterated)
+    print(f"Пресловљени метаподаци у датотеци 'content.opf'")
+
+
+def translit_pages(bk, html_parser, cyr):
+    for (id, href,) in bk.text_iter():
+        # Ugly hack to preserve &nbsp; in HTML file
+        # after encoding to UTF-8 for parser
+        source = bk.readfile(id).replace('&nbsp;', '&#9208;')
+        tree = etree.HTML(source.encode('utf-8'), html_parser)
+        transliterated = html_lat2cyr(tree, cyr, HTML_DOCTYPE)
+        bk.writefile(id, transliterated)
+        print(f"Пресловљена датотека '{href}'")
+
+
+def run(bk):
+    cyr = pycir.SerbCyr()
+    html_parser = etree.HTMLParser(remove_blank_text=True, remove_comments=False, encoding='utf-8')
+    xml_parser = etree.XMLParser(remove_blank_text=True, remove_comments=False, encoding='utf-8')
+
+    print("Пресловљавање ЕПУБ-а на српску ћирилицу...")
+    translit_toc(bk, xml_parser, cyr)
+    translit_metadata(bk, xml_parser, cyr)
+    translit_pages(bk, html_parser, cyr)
+    print("Завршено - не заборавите да 'ручно' замените: '⏸' са '&\u200Cnbsp;' у целом ЕПУБ-у!")
+    return 0
+
+
+def main():
+    print("Долазак у ову функцију није требало да се деси.\n")
+    return -1
+
+
+if __name__ == "__main__":
+    exit(main())
