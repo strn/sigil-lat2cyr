6d8cfed3ad728cf44e087aebc7d7ca2d39ae6ae5
Strn <strn@github.com>
2022-07-16 08:35:42 +0200

    Added UNIX shell script for making release archive

---
+++ make-sigil-plugin.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+RELEASE=${1:-1.0.0}
+DEST=lat2cyr
+TMPDESTDIR=/tmp/${DEST}
+
+mkdir ${TMPDESTDIR}
+cp -r . ${TMPDESTDIR}
+cd /tmp
+rm -rf ${DEST}/.git* ${DEST}*.zip
+sed -i -e "s/#VERSIONPLACEHOLDER#/${RELEASE}/g" ${DEST}/plugin.xml
+zip ${DEST}.zip -r ${DEST} -x ${DEST}/README.md ${DEST}/LICENSE ${DEST}/*sh
+unzip -t ${DEST}.zip
+rm -rf ${TMPDESTDIR}
+echo "Sigil plugin lat2cyr v${RELEASE} is ready in '/tmp/${DEST}.zip'"
